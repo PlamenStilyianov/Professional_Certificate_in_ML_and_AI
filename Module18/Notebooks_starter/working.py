135337380b08f9bb485b5dfb5dfcc3bf105e2d7d
Plamen Stilyianov <plamen.stilyianov@outlook.com>
2024-02-06 02:15:38 +0000

    Completed Module 18

---
+++ Module18/Notebooks_starter/working.py
@@ -0,0 +1,14 @@
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag, word_tokenize
+wnl = WordNetLemmatizer()
+sent = 'This is a foo bar sentence'
+pos_tag(word_tokenize(sent))
+
+for word, tag in pos_tag(word_tokenize(sent)):
+    wntag = tag[0].lower()
+    wntag = wntag if wntag in ['a', 'r', 'n', 'v'] else None
+    if not wntag:
+        lemma = word
+    else:
+        lemma = wnl.lemmatize(word, wntag)
+    print(lemma)
