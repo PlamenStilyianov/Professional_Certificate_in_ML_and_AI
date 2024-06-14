73c165177d9c9583ff0f0e312a206bfe43692fd3
Plamen Stilyianov <plamen_stilyianov@yahoo.com>
2024-06-14 20:00:06 +0100

    Changed the news server url

--- Module24/CapstoneProject/utils/live_news.py
+++ Module24/CapstoneProject/utils/live_news.py
@@ -8,7 +8,7 @@ from decouple import config
 warnings.filterwarnings("ignore")
 api_key = config("ALPACA_KEY")
 secret_key = config("ALPACA_SECRET")
-live_news_url = config("ALPACA_NEWS_WSS_URL")
+live_news_url = config("ALPACA_WSS_URL")
 
 wss_client = Stream(key_id=api_key, secret_key=secret_key, data_stream_url=URL(live_news_url))
 
